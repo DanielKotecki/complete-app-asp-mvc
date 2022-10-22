124b4fbf34464c7c5ba1c106bd947bb6487470e9
Daniel <daniel.kotecki98@gmail.com>
2022-10-22 16:05:29 +0200

    Add controllers and Database

---
+++ eTickets/Data/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using eTickets.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Actor_Movie>().HasKey(am => new
+            {
+                am.ActorId, am.MovieId
+            });
+            modelBuilder.Entity<Actor_Movie>().HasOne(m => m.Movie).WithMany(am => am.ActorMovies)
+                .HasForeignKey(x => x.MovieId);
+            modelBuilder.Entity<Actor_Movie>().HasOne(a => a.Actor).WithMany(m => m.ActorMovies)
+                .HasForeignKey(x => x.ActorId);
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Actor_Movie> Actors_Movies { get; set; }
+        public DbSet<Cinema> Cinemas { get; set; }
+        public DbSet<Producer> Producers { get; set; }
+      
+    }
+}
