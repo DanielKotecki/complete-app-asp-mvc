48111bccb1d1a1d2545aa3461e94966288f273d8
Daniel <daniel.kotecki98@gmail.com>
2022-11-06 22:25:56 +0100

    add view cinema Producer

---
+++ eTickets/Views/Cinemas/Index.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace eTickets.Views.Cinemas
+{
+    public class Index : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
