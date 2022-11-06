35f0dcbab880f6597776a88f27e4661e6d28c735
Daniel <daniel.kotecki98@gmail.com>
2022-11-06 16:51:49 +0100

    Adding Actor View

--- eTickets/Models/Actor.cs
+++ eTickets/Models/Actor.cs
@@ -6,8 +6,11 @@ namespace eTickets.Models
     public class Actor
     {
         [Key] public int Id { get; set; }
+        [Display(Name = "Profile Picture Url")]
         public string ProfilePictureURL { get; set; }
+        [Display(Name = "Full Name")]
         public string FullName { get; set; }
+        [Display(Name = "Biography")]
         public string Bio { get; set; }
         public List<Actor_Movie> ActorMovies { get; set; }
     }
