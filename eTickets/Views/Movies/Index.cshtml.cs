71c545b70cb123920c10badf0d06d5e6ec2c0c45
Daniel <daniel.kotecki98@gmail.com>
2022-11-07 23:03:48 +0100

    Add navbar,View Movies,delete home

---
+++ eTickets/Views/Movies/Index.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace eTickets.Views.Movies
+{
+    public class Index : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
